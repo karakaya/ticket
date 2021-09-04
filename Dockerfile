8d9f28834d3ec8458a0399d602363df123adb924
mkarakaya <mkarakaya604@gmail.com>
2021-09-04 18:40:15 +0300

    move database to .env and docker update

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:1.16-alpine AS builder
 
 WORKDIR /app
 COPY . .
-
+RUN cp .env.example .env
 RUN go mod download
 RUN CGO_ENABLED=0 go build -o main .
 #RUN apk add upx
--- config/Database.go
+++ config/Database.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,9 +13,24 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	db, err := gorm.Open(mysql.Open("root:password@tcp(localhost:3306)/ticket?charset=utf8mb4&parseTime=True&loc=Local"))
+	godotenv.Load(".env")
+	dbhost := goDotEnvVariable("DB_HOST")
+	dbport := goDotEnvVariable("DB_PORT")
+	dbname := goDotEnvVariable("DB_NAME")
+	dbuser := goDotEnvVariable("DB_USER")
+	dbpassword := goDotEnvVariable("DB_PASSWORD")
+	fmt.Println(dbhost)
+	db, err := gorm.Open(mysql.Open(dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"))
 	if err != nil {
 		panic(err)
 	}
 	DB = db
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("err loading .env file")
+	}
+	return os.Getenv(key)
 }
