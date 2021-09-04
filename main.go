fb67e8196b583f4bf44a28e4d7b8cacce1dba20b
mkarakaya <mkarakaya604@gmail.com>
2021-09-04 12:28:16 +0300

    remove static for now

--- main.go
+++ main.go
@@ -16,10 +16,6 @@ func main() {
 	//migration.InitAdmin()
 	r := mux.NewRouter().StrictSlash(true)
 	route.Routes(r)
-	r.
-		PathPrefix("/static/").
-		Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("."+"/static/"))))
-
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
 	}
