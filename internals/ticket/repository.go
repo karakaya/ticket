360286b53371d7d929597560169aab1aa48104ef
mkarakaya <40628802+karakaya@users.noreply.github.com>
2022-08-19 15:41:38 +0300

    adding repository skeleton

--- internals/ticket/repository.go
+++ internals/ticket/repository.go
@@ -1,1 +1,26 @@
 package ticket
+
+import "github.com/google/uuid"
+
+type Repository interface {
+	Create(ticket Ticket) (uuid.UUID, error)
+	Get(id uuid.UUID) (Ticket, error)
+	Delete(id uuid.UUID) error
+}
+
+type repository struct{}
+
+func (r repository) Get(id uuid.UUID) (Ticket, error) {
+	return Ticket{}, nil
+}
+
+func (r repository) Create(ticket Ticket) (uuid.UUID, error) {
+	return uuid.UUID{}, nil
+}
+func (r repository) Delete(id uuid.UUID) error {
+	return nil
+}
+
+func NewRepository() Repository {
+	return repository{}
+}
