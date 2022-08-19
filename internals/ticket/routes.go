32d6fc8cb99ea0bddb4208a4e5f81c0e9b4b2d00
mkarakaya <40628802+karakaya@users.noreply.github.com>
2022-08-19 17:09:45 +0300

    replacing routers

--- internals/ticket/routes.go
+++ internals/ticket/routes.go
@@ -1,1 +1,16 @@
 package ticket
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func RegisterHandlers(r *mux.Router, service Service) {
+	res := resource{service}
+
+}
+
+type resource struct {
+	ser Service
+}
+
+//TODO add endpoints
