096ab006940a74292e4ec1c824114b3297a5b4db
mkarakaya <40628802+karakaya@users.noreply.github.com>
2022-08-25 20:50:35 +0300

    update req/res

--- pkg/request/request.go
+++ pkg/request/request.go
@@ -16,6 +16,21 @@ type CreateTicketResponse struct {
 	ID uuid.UUID `json:"_id"`
 }
 
+type UpdateTicketRequest struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	Email string `json:"email"`
+}
+
+type UpdateTicketResponse struct {
+	ID        uuid.UUID `json:"_id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 type FindTicketResponse struct {
 	ID        uuid.UUID `json:"_id"`
 	Title     string    `json:"title"`
